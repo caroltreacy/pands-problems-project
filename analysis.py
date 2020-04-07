5f62f127b19810b61dd2f6788553505493f05cd6
caroltreacy <caroltreacy85@gmail.com>
2020-04-07 16:56:32 +0100

    started project

--- analysis.py
+++ analysis.py
@@ -1,3 +1,12 @@
+# write a program
 # outputs a summary of each variable to a single text file
 # saves a histogram of each variable to png files and
 # out puts a scatter plot of each pair of variables
+
+# insert variable
+
+import numpy as np
+import matplotlib.py as plt
+import seaborn as sns
+import pandas as pd 
+
